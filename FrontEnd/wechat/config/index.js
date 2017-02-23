1e916c4b37554e7da20c153b0e1fb85cbf3eccb3
Wilson <wilsonliuxyz@gmail.com>
2017-02-23 13:51:33 +0800

    第一版

--- FrontEnd/wechat/config/index.js
+++ FrontEnd/wechat/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
-    httpUrl: 'http://wilsonliu.cn/qualityOfGraduateDesigning/BackEnd/public/index.php/',
+    httpUrl: 'http://wilsonliu.cn/bishe/first/BackEnd/public/index.php/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
