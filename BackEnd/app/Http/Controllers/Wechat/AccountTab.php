47960fcf0ccf857acc2b60f206f490e3ab85d1bc
Wilson <wilsonliuxyz@gmail.com>
2017-03-04 19:01:33 +0800

    修复更新个人信息的BUG

--- BackEnd/app/Http/Controllers/Wechat/AccountTab.php
+++ BackEnd/app/Http/Controllers/Wechat/AccountTab.php
@@ -18,14 +18,7 @@ class AccountTab extends Controller
     }
     public function postModify()
     {
-        if($this->isTeacher()){
-            // 老师
-            $update = Input::only("intro","qq","email","phone");
-        } else if($this->getSessionInfo("type") == 2){
-          // 学生
-            $update = Input::only("intro","qq","email","phone");
-        }
-
+        $update = Input::only("intro","qq","email","phone");
         $this->getUser()->update($update);
         return $this->toast(1,"账户信息修改成功");
     }
